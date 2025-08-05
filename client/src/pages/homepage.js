b3b51842af5177122e3b13d281c89d87598d5305
Yash panchal <yashpanchal2905@gmail.com>
2025-08-06 00:25:18 +0530

    removed section5

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -3,7 +3,6 @@ import Section1 from "../components/section1";
 import Section2 from "../components/section2";
 import Section3 from "../components/section3";
 import Section4 from "../components/section4";
-import Section5 from "../components/section5";
 import Footer from "../components/footer";
 
 function Home() {
@@ -19,7 +18,6 @@ function Home() {
       <Section2 />
       <Section3 />
       <Section4 />
-      <Section5 />
       <Footer />
     </>
   );
